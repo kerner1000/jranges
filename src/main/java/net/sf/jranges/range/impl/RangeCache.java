24908d88e2bb377e054c9fd8ed15b9d91ef5e5a7
Kerner <alex.kerner.24@googlemail.com>
2010-11-20 00:49:24 +0100

    Add RangeCache.

---
+++ src/main/java/net/sf/jranges/range/impl/RangeCache.java
@@ -0,0 +1,72 @@
+package net.sf.jranges.range.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import net.sf.jranges.range.IntegerRange;
+
+class RangeCache implements Iterable<Collection<? extends IntegerRange>>,Iterator<Collection<? extends IntegerRange>>{
+	
+	final static int DEFAULT_ELEMENTS = 10000;
+	
+	final int elements;
+	
+	private final List<? extends IntegerRange> ranges;
+	
+	private volatile Collection<? extends IntegerRange> cache;
+	
+	private volatile int index = 0;
+	
+	RangeCache(final Collection<? extends IntegerRange> ranges, int elements){
+		this.elements = elements;
+		this.ranges = new ArrayList<IntegerRange>(ranges);
+		fill();
+	}
+	
+	RangeCache(final Collection<? extends IntegerRange> ranges){
+		this.elements = DEFAULT_ELEMENTS;
+		this.ranges = new ArrayList<IntegerRange>(ranges);
+		fill();
+	}
+	
+	private void fill(){
+		int end = index + elements;
+		if(end > ranges.size()-1)
+			end = ranges.size();
+		cache = new ArrayList<IntegerRange>(ranges.subList(index, end));
+		index += elements;
+	}
+
+	/** 
+	 * 
+	 */
+	public boolean hasNext() {
+//		System.err.println(index);
+		return (index <= ranges.size());
+	}
+
+	/** 
+	 * 
+	 */
+	public synchronized Collection<? extends IntegerRange> next() {
+		final Collection<? extends IntegerRange> result = new ArrayList<IntegerRange>(cache);
+		fill();
+		return result;
+	}
+
+	/** 
+	 * 
+	 */
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+	/** 
+	 * 
+	 */
+	public Iterator<Collection<? extends IntegerRange>> iterator() {
+		return this;
+	}
+}
---
+++ src/test/java/net/sf/jranges/range/impl/TestRangeCache.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package net.sf.jranges.range.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import net.sf.jranges.range.IntegerRange;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
+ * @version 2010-11-18
+ *
+ */
+public class TestRangeCache {
+	
+	RangeCache cache;
+	IntegerRange range1;
+	IntegerRange range2;
+	IntegerRange range3;
+	IntegerRange range4;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link net.sf.kerner.commons.range.impl.RangeCache#next()}.
+	 */
+	@Test
+	public final void testNext() {
+		ArrayList<IntegerRange> range = new ArrayList<IntegerRange>();
+		range1 = new DummyRange(1,10);
+			range2 = new DummyRange(11,20);
+				range3 = new DummyRange(21,30);
+					range4 = new DummyRange(31,40);
+		range.add(range1);
+		range.add(range2);
+		range.add(range3);
+		range.add(range4);
+		cache = new RangeCache(range, 2);
+		assertTrue(cache.hasNext());
+		assertEquals(Arrays.asList(range1, range2), cache.next());
+		assertTrue(cache.hasNext());
+		assertEquals(Arrays.asList(range3, range4), cache.next());
+		assertFalse(cache.hasNext());
+	}
+
+}
