ac0c8f1fd61be3581b37b103a8bacb34175633fc
Kerner <alex.kerner.24@googlemail.com>
2011-04-05 17:24:04 +0200

    [API-Break] Move LongRange to package range.longrange.

--- src/main/java/net/sf/jranges/range/longrange/LongRange.java
+++ src/main/java/net/sf/jranges/range/longrange/LongRange.java
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
  ***********************************************************************/
 
-package net.sf.jranges.range.integer;
+package net.sf.jranges.range.longrange;
 
 import net.sf.jranges.range.Range;
 import net.sf.jranges.range.RangeException;
