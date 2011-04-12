77066fa1a01f7f330f8fe7d4a5bce6ef6665bcac
Kerner <alex.kerner.24@googlemail.com>
2011-04-12 16:56:06 +0200

    [API-Break] Refactor DummyDoubleRange to doublerange.impl.

--- src/main/java/net/sf/jranges/range/doublerange/impl/DummyDoubleRange.java
+++ src/main/java/net/sf/jranges/range/doublerange/impl/DummyDoubleRange.java
@@ -13,9 +13,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
  ***********************************************************************/
 
-package net.sf.jranges.range.doublerange;
+package net.sf.jranges.range.doublerange.impl;
 
 import net.sf.jranges.range.RangeException;
+import net.sf.jranges.range.doublerange.DoubleRange;
 
 /**
  * 
