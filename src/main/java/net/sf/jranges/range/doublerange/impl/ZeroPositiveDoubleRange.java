0553c6c8ad6277ade3b2dbe413c7ea1316665d81
Kerner <alex.kerner.24@googlemail.com>
2011-05-10 10:03:03 +0200

    Feature: Introduce ZeroPositoveDoubleRange#(String start, String, stop, String interval).

--- src/main/java/net/sf/jranges/range/doublerange/impl/ZeroPositiveDoubleRange.java
+++ src/main/java/net/sf/jranges/range/doublerange/impl/ZeroPositiveDoubleRange.java
@@ -11,6 +11,10 @@ public class ZeroPositiveDoubleRange extends AbstractDoubleRange {
 	public ZeroPositiveDoubleRange(double start, double stop, double interval) throws RangeException {
 		super(start, stop, 0, Double.MAX_VALUE, interval);
 	}
+	
+	public ZeroPositiveDoubleRange(String start, String stop, String interval) throws RangeException, NumberFormatException {
+		this(Double.parseDouble(start), Double.parseDouble(stop), Double.parseDouble(interval));
+	}
 
 	@Override
 	protected ZeroPositiveDoubleRange newInstange(double start, double stop, double limit1, double limit2)
