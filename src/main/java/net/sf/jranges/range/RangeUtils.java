ba7ca8ebc39b787ee9acba16aeb0de834aa09947
Kerner <alex.kerner.24@googlemail.com>
2011-07-12 15:57:13 +0200

    [Fix] RangeUtils#doForAllInRange(): fix rounding error.

--- src/main/java/net/sf/jranges/range/RangeUtils.java
+++ src/main/java/net/sf/jranges/range/RangeUtils.java
@@ -2,6 +2,7 @@ package net.sf.jranges.range;
 
 import net.sf.jranges.range.doublerange.DoubleRange;
 import net.sf.jranges.range.integer.IntegerRange;
+import net.sf.kerner.utils.math.MathUtils;
 
 public class RangeUtils {
 	
@@ -22,7 +23,7 @@ public class RangeUtils {
 	}
 	
 	public static void doForAllInRange(DoubleRange range, DoubleRangeTask task){
-		for(double i = range.getStart(); i <= range.getStop(); i+= range.getInterval()){
+		for(double i = range.getStart(); MathUtils.round(i, 6) <= range.getStop(); i+= range.getInterval()){
 			task.call(i);
 		}
 	}
