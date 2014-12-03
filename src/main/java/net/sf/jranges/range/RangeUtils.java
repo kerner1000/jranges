35a59eb8284d53aa9e397cb3dea268944c994210
Kerner <alexanderkerner24@gmail.com>
2014-12-03 18:27:16 +0100

    Fix: Add null check.

--- src/main/java/net/sf/jranges/range/RangeUtils.java
+++ src/main/java/net/sf/jranges/range/RangeUtils.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2010-2014 Alexander Kerner. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,18 +32,23 @@ public class RangeUtils {
     }
 
     public static void doForAllInRange(final DoubleRange range, final DoubleRangeTask task) {
-        for (double i = range.getStart(); UtilMath.round(i, 6) <= range.getStop(); i += range.getInterval()) {
+        for (double i = range.getStart(); UtilMath.round(i, 6) <= range.getStop(); i += range
+                .getInterval()) {
             task.call(i);
         }
     }
 
     public static void doForAllInRange(final IntegerRange range, final IntegerRangeTask task) {
+        if (range == null || task == null) {
+            throw new NullPointerException();
+        }
         for (int i = range.getStart(); i <= range.getStop(); i += range.getInterval()) {
             task.call(i);
         }
     }
 
-    public static boolean includesAll(final IntegerRange range, final Collection<? extends Integer> positions) {
+    public static boolean includesAll(final IntegerRange range,
+            final Collection<? extends Integer> positions) {
         for (final int i : positions) {
             if (range.includes(i)) {
                 // ok
